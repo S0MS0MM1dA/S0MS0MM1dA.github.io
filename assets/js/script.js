ad0202523934cf07bbb3b66b72c1c5c30def5153
S0MS0MM1dA <durjoypodder76@gmail.com>
2026-02-13 14:42:15 +0600

    Added: Experience section

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,16 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
       .scrollIntoView({ behavior: "smooth" });
   });
 });
+
+const tabs = document.querySelectorAll(".tab");
+const contents = document.querySelectorAll(".tab-content");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((btn) => btn.classList.remove("active"));
+    contents.forEach((content) => content.classList.remove("active"));
+
+    tab.classList.add("active");
+    document.getElementById(tab.dataset.tab).classList.add("active");
+  });
+});
